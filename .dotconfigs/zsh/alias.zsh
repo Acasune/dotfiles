46c8143ab7a4612e8ef8d55a3d4b0edb78032504
acasune <tttt4aaaa@gmail.com>
2023-11-11 15:24:27 +0900

    fix dotfiles

--- .dotconfigs/zsh/alias.zsh
+++
@@ -1,9 +0,0 @@
-#==============================================================#
-##          Aliases                                           ##
-#==============================================================#
-
-# ls
-alias la='ls -aF --color=auto'
-alias lla='ls -alF --color=auto'
-alias ls='ls --color=auto'
-alias ll='ls -l --color=auto'

bb7b32a3e1e098260294ac5fcb9e7a229b0c1771
acasune <tttt4aaaa@gmail.com>
2024-01-27 12:16:52 +0900

    replace ls with exa

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -2,17 +2,11 @@
 ##          Aliases                                           ##
 #==============================================================#
 
-# ls
-alias la='ls -aF --color=auto'
-alias lla='ls -alF --color=auto'
-alias ls='ls --color=auto'
-alias ll='ls -l --color=auto'
-
 # exa
-alias ea='exa -aF'
-alias eea='exa -alF'
-alias es='exa'
-alias ee='exa -l'
+alias ls='exa'
+alias la='exa -aF'
+alias lla='exa -alF'
+alias ll='exa -l'
 
 # others
 alias pbcopy='xsel --clipboard --input'
